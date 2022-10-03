16181aec6bb2a2a4c48a7ff43e3415ca350406a2
YasminFLS <yasmin_fl_silva@estudante.sc.senai.br>
2022-10-03 10:43:18 -0300

    atualização

---
+++ inserir.php
@@ -0,0 +1,14 @@
+<?php
+    include("conexao.php");
+
+    $nome = $_POST["nome"];
+    $senha = MD5($_POST["senha"]);
+    $telefone = $_POST["telefone"];
+    $comando = $pdo -> prepare("INSERT INTO usuario(nome_usuario,senha_usuario,telefone_usuario) VALUES(:nome,:senha,:telefone)");
+    $comando->bindValue(":nome",$nome);                                     
+    $comando->bindValue(":senha",$senha);
+    $comando->bindValue(":telefone",$telefone);  
+    $comando->execute();                               
+
+    header("Location:Início(com conta).html");
+?>
