1ff71f478f4a0eefb94a5da8412ac429bc4da048
YasminFLS <yasmin_fl_silva@estudante.sc.senai.br>
2022-10-17 11:23:44 -0300

    update perfil e inicio

--- inserir.php
+++ inserir.php
@@ -4,10 +4,14 @@
     $nome = $_POST["nome"];
     $senha = MD5($_POST["senha"]);
     $telefone = $_POST["telefone"];
-    $comando = $pdo -> prepare("INSERT INTO usuario(nome_usuario,senha_usuario,telefone_usuario) VALUES(:nome,:senha,:telefone)");
+    $imagem = $_FILES['imagem'];
+    $conteudo = file_get_contents($imagem['tmp_name']);
+    $base64 = "data:".$extensao.";base64,".base64_encode($conteudo);
+    $comando = $pdo -> prepare("INSERT INTO usuario(nome_usuario,senha_usuario,telefone_usuario,foto_usuario) VALUES(:nome,:senha,:telefone,:conteudo)");
     $comando->bindValue(":nome",$nome);                                     
     $comando->bindValue(":senha",$senha);
-    $comando->bindValue(":telefone",$telefone);  
+    $comando->bindValue(":telefone",$telefone);
+    $comando->bindValue(":conteudo", $base64);
     $comando->execute();                               
 
     header("location: Login.html");
