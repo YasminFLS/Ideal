769f23af15f71cef0cf1592866840d7ee6d673e0
YasminFLS <yasmin_fl_silva@estudante.sc.senai.br>
2022-10-24 11:10:15 -0300

    update

---
+++ listar_profissionais.php
@@ -0,0 +1,17 @@
+<?php
+    include("conexao.php");
+
+    $comando = $pdo -> prepare("SELECT nome_profissional FROM profissional WHERE id_profissional = :id_profissional");
+
+    $comando->bindValue(":id_profissional", $_SESSION["id_profissional"]);
+    $comando->execute();
+
+    if($comando->rowCount() >= 1)
+{
+
+    $Agendamentos_adm = $comando->fetch();
+
+
+    unset($comando);
+    unset($pdo);
+?>
