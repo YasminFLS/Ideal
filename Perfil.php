c7f1291a0b20342cf253127d3049eb08916ab9ec
YasminFLS <yasmin_fl_silva@estudante.sc.senai.br>
2022-10-25 10:23:05 -0300

    Update links

--- Perfil.php
+++ Perfil.php
@@ -12,9 +12,9 @@
 
 
     <div class="cabecalho">
-        <img class="logo" src="imagens/logo.png" height="40px">
-        <img class="agendamento" src="imagens/Agendamento.png" height="40px">
-        <img class="quiz" src="imagens/quiz.png" height="40px">
+    <a href="inicio_com_conta.html"><img class="logo" src="imagens/logo.png" height="40px"></a>
+        <a href="Agendamentos.html"><img class="agendamento" src="imagens/Agendamento.png" height="40px"></a>
+        <a href="quiz_com_conta.php"><img class="quiz" src="imagens/quiz.png" height="40px"></a>
         <img class="ima" src="imagens/imagem.jpg" height="40px">
     </div>
 
@@ -33,7 +33,7 @@
     </div>
 
     <input class="botao_hist" type="button" value="Histórico do Quiz">
-    <input class="botao_agend" type="button" value="Histórico de Agendamentos">
+    <a href="AgendamentosPrevios.php"><input class="botao_agend" type="button" value="Histórico de Agendamentos"></a>
     <?php
         session_start();
         if ($_SESSION['tipo_usuario']==1){
